4fe564698cdd58d7f7188f84f65c98ca1fe27974
VadimBB <vadim.bogatyreff@gmail.com>
2020-02-28 00:08:38 +0300

    VDUDUD-54 - Восстановления пароля. Пустые пароли

--- src/test/java/ru/vdudvdud/objects/vdudvdud/pages/RestorePasswordConfirmationPage.java
+++ src/test/java/ru/vdudvdud/objects/vdudvdud/pages/RestorePasswordConfirmationPage.java
@@ -6,6 +6,7 @@ import static ru.vdudvdud.testdata.enums.urls.BaseUrls.FORGOT_PASSWORD;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
+import org.testng.Assert;
 import ru.vdudvdud.adaptors.selenide.base.BasePage;
 import ru.vdudvdud.testdata.enums.localization.RestorePasswordLocalization;
 
@@ -49,6 +50,17 @@ public class RestorePasswordConfirmationPage extends BasePage {
             .shouldHave(Condition.exactText(RestorePasswordLocalization.DIFFERENT_PASSWORDS_MESSAGE.getValue()));
     }
 
+    public void checkThatDifferentPasswordsErrorMsgIsVisible() {
+        Assert.assertEquals(RestorePasswordLocalization.DIFFERENT_PASSWORDS_MESSAGE.getValue(),
+            ERROR_MSG_UNDER_REPEAT_PASSWORD.getText(),
+            "Ожидаемое сообщение о несовпадение паролей не соответвует фактическому");
+    }
+
+    public void checkThatEmptyPasswordsErrorMsgIsVisible() {
+        Assert.assertEquals(RestorePasswordLocalization.EMPTY_PASSWORDS_MESSAGE.getValue(),
+            ERROR_MSG_UNDER_PASSWORD.getText(),
+            "Ожидаемое сообщение о пустых паролях не соответвует фактическому");
+    }
 
     public void fillPassword(String password) {
         actions().sendKeys(PASSWORD.toWebElement(), password).build().perform();
--- src/test/java/ru/vdudvdud/testdata/enums/localization/RestorePasswordLocalization.java
+++ src/test/java/ru/vdudvdud/testdata/enums/localization/RestorePasswordLocalization.java
@@ -4,7 +4,8 @@ import ru.vdudvdud.adaptors.selenide.Localization;
 
 public enum RestorePasswordLocalization implements ILocalizedValue {
     INCORRECT_EMAIL_MESSAGE("restore_password.restore_form.invalid_email_message"),
-    DIFFERENT_PASSWORDS_MESSAGE("restore_password.restore_form.different_passwords_message");
+    DIFFERENT_PASSWORDS_MESSAGE("restore_password.restore_form.different_passwords_message"),
+    EMPTY_PASSWORDS_MESSAGE("restore_password.restore_form.empty_passwords_message");
 
     private String propKey;
 
