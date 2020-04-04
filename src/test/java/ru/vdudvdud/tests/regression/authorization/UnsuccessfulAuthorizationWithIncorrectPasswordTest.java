e7fd3e48583cf37649f4ed83fd148ac98115d87a
Evgeniy.Mustafin <Evgeniy.Mustafin@acronis.com>
2020-04-04 16:46:36 +0300

    Conflicts resolution

--- src/test/java/ru/vdudvdud/tests/regression/authorization/UnsuccessfulAuthorizationWithIncorrectPasswordTest.java
+++ src/test/java/ru/vdudvdud/tests/regression/authorization/UnsuccessfulAuthorizationWithIncorrectPasswordTest.java
@@ -4,9 +4,10 @@ import io.qameta.allure.Link;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.vdudvdud.adaptors.selenide.base.BaseTest;
-import ru.vdudvdud.steps.HeaderSteps;
-import ru.vdudvdud.steps.SignInSteps;
-import ru.vdudvdud.steps.scenarios.RegistrationScenarios;
+import ru.vdudvdud.steps.vdudvdud.HeaderSteps;
+import ru.vdudvdud.steps.vdudvdud.SignInSteps;
+import ru.vdudvdud.steps.vdudvdud.scenarios.RegistrationScenarios;
+import ru.vdudvdud.testdata.constants.StringConstants;
 import ru.vdudvdud.testdata.creators.UsersCreator;
 import ru.vdudvdud.testdata.models.essences.User;
 import ru.vdudvdud.testdata.utils.TestDataProvider;
@@ -14,7 +15,7 @@ import ru.vdudvdud.testdata.utils.TestDataProvider;
 public class UnsuccessfulAuthorizationWithIncorrectPasswordTest extends BaseTest {
     private HeaderSteps headerSteps = new HeaderSteps();
     private SignInSteps signInSteps = new SignInSteps();
-    private RegistrationScenarios registrationScenarios= new RegistrationScenarios();
+    private RegistrationScenarios registrationScenarios = new RegistrationScenarios();
 
 
     private User user;
@@ -23,7 +24,7 @@ public class UnsuccessfulAuthorizationWithIncorrectPasswordTest extends BaseTest
     @BeforeMethod
     public void readParams() {
         user = UsersCreator.createRandomUser();
-        incorrectPassword = TestDataProvider.generateRandomString();
+        incorrectPassword = TestDataProvider.generateRandomString(StringConstants.BASE_RANDOM_STRING_LENGTH);
         registrationScenarios.registrationAndLogout(user);
     }
 
