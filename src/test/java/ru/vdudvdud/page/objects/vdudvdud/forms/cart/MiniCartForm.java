a96e0b5567f8c8404269956829cc286b2942a6ae
VadimBB <vadim.bogatyreff@gmail.com>
2020-04-28 19:29:10 +0300

    fix

--- src/test/java/ru/vdudvdud/page/objects/vdudvdud/forms/cart/MiniCartForm.java
+++ src/test/java/ru/vdudvdud/page/objects/vdudvdud/forms/cart/MiniCartForm.java
@@ -25,6 +25,7 @@ public class MiniCartForm extends PageObject {
     private final String ITEM_COUNT = ".//span[@class='mini-cart__quantity']";
     private final String ITEM_PRICE = ".//div[@class='mini-cart__price']";
     private final String ITEM_CLOSE = ".//div[@class='mini-cart__close']";
+    private final String ITEM = ".//li[@class='mini-cart__item']";
     private final Cart cart = Cart.getInstance();
 
 
@@ -45,7 +46,7 @@ public class MiniCartForm extends PageObject {
     }
 
     public ElementsCollection getProductElements() {
-        return getMainElement().$$x(".//li[@class='mini-cart__item']");
+        return getMainElement().$$x(ITEM);
     }
 
     public void removeProductFromMiniCart(MiniCartProduct miniCartProduct) {
